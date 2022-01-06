ac3700d1d51a4f782c251a6e4abc701ec9df6126
SINEdowskY <kub200105@gmail.com>
2022-01-06 18:23:35 +0100

    Create full-working all features

--- Graph.h
+++ Graph.h
@@ -13,9 +13,13 @@ using namespace std;
 class Graph {
 
 private:
-    vector<vector<int>> matrix;
-    int edgeAmount, verticeAmount;
+    vector<vector<int>> matrix; //zmienna zapisujaca macierz
+    vector<vector<int>> edgeMap; //zmienna zapisujaca macierz krawedzi {source, destination}
+    vector<int> loops; //zmienna zapisujaca tablice petli
+    vector<int> isolated; //zmienna zapisujaca tablice wierzcholkow izolowanych
+    int edgeAmount, verticeAmount; //zmienne zapisujace (kolejno) ilosc krawedzi i ilosc wierzcholkow
 
+    //funkcja sprawdzajaca czy podana macierz jest macierza incydencji
     static bool matrixValidation(vector<vector<int>> matrix){
         bool result = false;
         int minus, plus;
@@ -31,7 +35,7 @@ private:
                     minus++;
                 }
             }
-            if((plus == 1 || minus == 1) && plus >= minus){
+            if((plus == 1 || minus == 1) && plus >= minus && plus == 1){
                 result = true;
             }
             else {
@@ -41,14 +45,40 @@ private:
         }
         return result;
     }
+    //funckja generujaca macierz krawedzi
+    void edgeMapping(){
+        int plusPosition, minusPosition;
+
+        for(int i = 0; i < this->edgeAmount; i++){
+            plusPosition = -1;
+            minusPosition = -1;
+
+            for(int j = 0; j < this->verticeAmount; j++){
+                if(this->matrix[j][i] == 1){
+                    plusPosition = j;
+                }
+                else if(this->matrix[j][i] == -1){
+                    minusPosition = j;
+                }
+
+            }
+            this->edgeMap.push_back({plusPosition,minusPosition});
+
+        }
+    }
 
 public:
+
+    //konstruktor
     Graph(vector<vector<int>> matrix){
 
         if(matrixValidation(matrix)){
             this->matrix = matrix;
-            this->edgeAmount = matrix.size();
-            this->verticeAmount = matrix[0].size();
+            this->edgeAmount = matrix[0].size();
+            this->verticeAmount = matrix.size();
+            this->loops = this->getLoops();
+            this->isolated = this->getIsolatedVertices();
+            edgeMapping();
         }
         else{
             cout << "Macierz nie spelnia wymagan do bycia macierza incyndecji"<<endl;
@@ -56,64 +86,81 @@ public:
 
     }
 
-    vector<int,vector<int>> getVertexNeighbors(){
-    }
-    //vertice [0,0,0,...,0] done
-    vector<string> getIsolatedVertices(){
-        vector<string> result;
-        int index = 1;
-        bool test;
+    // struktura roznowartosciowa dla vectora
+    struct int_vector{
+        int num;
+        vector<int> arr;
+    };
 
-        for(vector<int> el: this->matrix){
-            test = false;
-            for(int i  = 0; i < el.size(); i++){
-                if(el[i] != 0){
-                    break;
+    // Podpunkt 1)
+    vector<int_vector> getVerticeNeighbors(){
+        vector<int_vector> result;
+        vector<int> neighbours;
+            for(int i = 0; i < this->verticeAmount; i++){
+                for(int j = 0; j < this->edgeAmount; j++){
+                    if(this->matrix[i][j] == 1){
+                        for(int k = 0; k < this->verticeAmount; k++){
+                            if(matrix[k][j] == -1){
+                                neighbours.push_back(k);
+                                break;
+                            }
+                        }
+                    }
                 }
-                if(i == el.size()-1){
-                    test = true;
+                result.push_back({i,neighbours});
+                neighbours.clear();
+            }
+
+
+        return result;
+    }
+
+    // Podpunkt 2)
+    vector<int> getNeighborsOfAll(){
+        vector<int> result;
+        int sum;
+
+        if(isolated.empty()){
+            for(int i = 0; i < verticeAmount; i++){
+                sum = 0;
+                for(int j = 0; j < edgeAmount; j++){
+                    for(int el : this->loops){
+                        if(j != el){
+                            if(this->matrix[i][j] == 1){
+                                sum++;
+                            }
+                        }
+                    }
+                }
+                if(sum == this->verticeAmount){
+                    result.push_back(i);
                 }
             }
-            if(test){
-                result.push_back(string("Vertice")+ to_string(index));
-            }
-            index++;
         }
 
-        if(result.empty()){
-            result.push_back("None vertices are isolated");
+        return result;
+    }
+
+    // Podpunkt 3)
+    vector<vector<int>> getVerticesOutdegrees(){
+        vector<vector<int>> result;
+        int count = 0;
+        int index = 1;
+
+        for(vector<int> el: this->matrix){
+            for(int cell: el){
+                if(cell == 1){
+                    count++;
+                }
+            }
+            result.push_back({index, count});
+            index++;
+            count = 0;
         }
         return result;
     }
-    // amount of (1 or -1) == edge amount
-    vector<string> getNeighborsOfAll(){
-        vector<string> result;
-        int index = 1;
-        string str;
-        bool test;
 
-        for(vector<int> el: this->matrix){
-            test = false;
-            for(int i = 0; i < el.size(); i++){
-                if(el[i] == 0){
-                   break;
-                }
-                if(i == el.size()-1){
-                    test = true;
-                }
-            }
-            if(test){
-                result.push_back(string("Vertice ")+ to_string(index));
-            }
-            index++;
-        }
-
-        if(result.empty()){
-            result.push_back("None of vertices are neighbor of all");
-        }
-        return result;
-    }
-    //amount of -1 in vertice done
+    // Podpunkt 4)
     vector<vector<int>> getVerticesIndegrees(){
         vector<vector<int>> result;
         int count = 0;
@@ -131,32 +178,87 @@ public:
         }
         return result;
     }
-    // amount of 1 in vertice done
-    vector<vector<int>> getVerticesOutdegrees(){
-        vector<vector<int>> result;
-        int count = 0;
-        int index = 1;
 
-        for(const vector<int>& el: this->matrix){
-            for(int cell: el){
-                if(cell == 1){
-                    count++;
+    // Podpunkt 5)
+    vector<int> getIsolatedVertices(){
+        vector<int> result;
+        int index = 1;
+        bool test;
+
+        for(vector<int> el: this->matrix){
+            test = false;
+            for(int i  = 0; i < el.size(); i++){
+                if(el[i] != 0){
+                    break;
+                }
+                if(i == el.size()-1){
+                    test = true;
                 }
             }
-            result.push_back({index, count});
+            if(test){
+                result.push_back(index);
+            }
             index++;
-            count = 0;
         }
+
         return result;
     }
 
-    vector<string> getBidirectionalEdges(){
-
-    }
-    // edge where 1 distinct
+    // Podpunkt 6)
     vector<int> getLoops(){
+        vector<int> result;
+        int zero, plus;
 
+        for(int i = 0; i < matrix[0].size();i++){
+            zero = 0;
+            plus = 0;
+            for(auto & el : matrix){
+                if(el[i] == 0){
+                    zero++;
+                }
+                else if(el[i] == 1){
+                    plus++;
+                }
+            }
+            if(plus+zero==this->verticeAmount){
+                result.push_back(i);
+            }
+        }
+
+        return result;
     }
+
+    // Podpunkt 7)
+    vector<vector<int>> getBidirectionalEdges(){
+        vector<vector<int>> result;
+        int source, destiantion;
+        bool check = false;
+
+        for(int i = 0; i <this->edgeMap.size(); i++){
+            source = this->edgeMap[i][0];
+            destiantion = this->edgeMap[i][1];
+            for(int j = 0; j < this->edgeMap.size(); j++){
+                if(source == this->edgeMap[j][1] && destiantion == this->edgeMap[j][0]){
+                    if(!result.empty()){
+                        for(auto el: result){
+                            if(el[0] != destiantion && el[1] != source){
+                                check = true;
+                            }
+                        }
+                        if(!check){
+                            result.push_back({i,j});
+                        }
+                    }
+                    else{
+                        result.push_back({i,j});
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
 };
 
 
--- main.cpp
+++ main.cpp
@@ -7,14 +7,26 @@ int main() {
             {-1,0,-1,0,0,0},
             {0,-1,1,1,-1,0},
             {0,0,0,-1,1,0},
+            {0,0,0,0,0,0}
     };
 
     Graph graph(A);
 
-    vector<vector<int>> x = graph.getVerticesOutdegrees();
-    for(auto el: x){
-        cout << "Vertice " << el[0] << ", "<< el[1] <<endl;
-    }
 
+    vector<vector<int>> y = graph.getBidirectionalEdges();
+
+    for(auto el: y){
+        cout << el[0] << " : " << el[1]<< endl;
+    }
+/*
+    vector<Graph::int_vector> x = graph.getVerticeNeighbors();
+    for(auto el: x){
+        cout << el.num<<": ";
+        for(auto cell: el.arr){
+            cout << cell << ", ";
+        }
+        cout <<endl;
+    }
+*/
     return 0;
 }
